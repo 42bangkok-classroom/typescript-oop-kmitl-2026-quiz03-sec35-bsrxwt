017a3195a4020194bcf8efb6c22b10c9e18e7221
bsrxwt <sorawit281249@gmail.com>
2026-02-16 09:42:17 +0700

    04

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,19 @@
-export class User {}
+export class User {
+    public username:string = "";
+    private password:string = "";
+    private LOGIN_ATTEMPTS:number = 0;
+    constructor(usernames:string,passwords:string){
+        this.username = usernames;
+        this.password = passwords;
+    }
+    public login(password:string):boolean{
+        this.LOGIN_ATTEMPTS++
+        return this.validatePassword(password);
+    }
+    private validatePassword(password:string):boolean{
+        return this.password === password;
+    }
+    public getLoginAttempts():number{
+        return this.LOGIN_ATTEMPTS;
+    }
+}
