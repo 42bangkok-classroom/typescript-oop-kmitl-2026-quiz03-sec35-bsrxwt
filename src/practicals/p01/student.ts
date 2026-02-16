b7246ca51bc921e362fd7fb1eea4f948474c69c3
bsrxwt <sorawit281249@gmail.com>
2026-02-16 09:09:47 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,7 @@
-export class Student {}
+export class Student {
+    firstname:string ="";
+    lastname:string = "";
+    printName(){
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+}
