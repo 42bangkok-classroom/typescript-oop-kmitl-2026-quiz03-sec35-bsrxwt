6b1c77037d3629d08b36cfcd23056bb328c7ad0a
bsrxwt <sorawit281249@gmail.com>
2026-02-16 09:15:20 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,15 @@
-export class Person {}
+export class Person {
+    firstname:string ="";
+    lastname:string = "";
+    private age = 0;
+    static COUNTRY = "Thailand";
+    setAge(ages:number){
+        this.age = ages;
+    }
+    getAge(){
+        return this.age;
+    }
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`
+    }
+}
